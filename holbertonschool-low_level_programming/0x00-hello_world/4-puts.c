1337a2071e23fecd52807caa32584ae1bf51d26c
Rodolfo Vazquez <1678@holbertonschool.com>
2020-02-06 00:43:16 +0000

    addin 3 adn 4

---
+++ holbertonschool-low_level_programming/0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Uses put to add piece of quote
+ * Return: Always 0 (ok)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
